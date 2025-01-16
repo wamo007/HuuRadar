07edf0961db04ed6d9583f61b963aeae2314786d
wamo007 <shamil010@hotmail.com>
2025-01-16 12:14:04 +0100

    fixups

--- server/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const userAuth = require('../middleware/userAuth')
-const getUserData = require('../controllers/userController')
-
-const userRouter = Router()
-
-userRouter.get('/data', userAuth, getUserData)
-
-module.exports = userRouter
